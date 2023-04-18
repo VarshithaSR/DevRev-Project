508cfe01b1a68218707cca2fbaffe8b0ec092f75
VarshithaSR <76736968+VarshithaSR@users.noreply.github.com>
2023-04-18 18:06:34 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import "../styles/home.css";
 import { Container, Row, Col } from "reactstrap";
 import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
-import heroVideo from "../assets/images/hero-video.mp4";
+import heroVideo from "../assets/images/hero-img02.jpg";
 
 import Subtitle from "./../shared/Subtitle";
 
